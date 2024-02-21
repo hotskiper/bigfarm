9c24e5a6f2c0aa3f920a66a9cd173bf7c16d69a8
zdx <380311869@qq.com>
2024-02-21 17:12:46 +0800

    2

--- packages/react/lib/atoms/Margin/Margin.d.ts
+++ packages/react/lib/atoms/Margin/Margin.d.ts
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Spacing } from '@bigfarm/foundation'
+import React from 'react';
+import { Spacing } from '@bigfarm/foundation';
 interface MarginProps {
-  space?: keyof typeof Spacing
-  children: any
-  left?: boolean
-  right?: boolean
-  top?: boolean
-  bottom?: boolean
+    space?: keyof typeof Spacing;
+    children: any;
+    left?: boolean;
+    right?: boolean;
+    top?: boolean;
+    bottom?: boolean;
 }
-declare const Margin: React.FC<MarginProps>
-export default Margin
+declare const Margin: React.FC<MarginProps>;
+export default Margin;
