bb87b10860817e630bedb6ebef6473f92f9475e5
zdx <380311869@qq.com>
2024-02-22 11:53:03 +0800

    delete lib

--- packages/react/lib/atoms/Text/Text.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Text = ({ children }) => {
-    return React.createElement("div", null, children);
-};
-
-export { Text as default };
-//# sourceMappingURL=Text.js.map
