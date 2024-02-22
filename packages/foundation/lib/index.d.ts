1cc26472aa039a1ed23539344acb2af36e424bed
zdx <380311869@qq.com>
2024-02-22 11:53:03 +0800

    delete lib

--- packages/foundation/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import FontSizes from './FontSize';
-import Spacing from './Spacing';
-export { FontSizes, Spacing };
--- packages/react/lib/atoms/Text/Text.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-interface TextProps {
-    children?: string;
-}
-declare const Text: React.FC<TextProps>;
-export default Text;
